f440744ea3d4393ac7033d8e2e53cc444c3256b5
Sanjarbek Saminjonov <sanjarbek@saminjonov.uz>
2024-05-21 17:22:48 +0500

    v2

---
+++ drive.py
@@ -0,0 +1,56 @@
+# https://developers.google.com/drive/api/quickstart/python
+# https://ragug.medium.com/how-to-upload-files-using-the-google-drive-api-in-python-ebefdfd63eab
+# https://developers.google.com/drive/api/guides/manage-uploads#python
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+
+def get_service():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json",
+                SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    service = build("drive", "v3", credentials=creds)
+    return service
+
+
+def file_upload(file_name: str, mimetype: str):
+    service = get_service()
+    file_metadata = {"name": file_name}
+    media = MediaFileUpload(
+        file_name,
+        mimetype=mimetype,
+    )
+    file = (
+        service.files()
+        .create(body=file_metadata, media_body=media, fields="id")
+        .execute()
+    )
+    return file.get("id")
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import os
-
 import environs
-import requests
 
+from drive import file_upload
+
+# get env object
 env = environs.Env()
 env.read_env()
 
-bot_token = env.str("BOT_TOKEN")
-chat_id = env.str("CHAT_ID")
+# get variables
+BOT_TOKEN = env.str("BOT_TOKEN")
+CHAT_ID = env.str("CHAT_ID")
 current_dir = env.str("CURRENT_DIR")
 db_name = env.str("DB_NAME")
 db_user = env.str("DB_USER")
 backup_name = f"{db_name}_{db_user}.sql.gz"
 
+# change dir to project root
 os.chdir(current_dir)
+
+# dump database
 command = f"docker exec -t {db_name} pg_dumpall -c -U {db_user} | gzip > {backup_name}"
 os.system(command)
 
-file_path = os.path.join(current_dir, backup_name)
-with open(file_path, "rb") as file:
-    res = requests.post(
-        url=f"https://api.telegram.org/bot{bot_token}/sendDocument",
-        data={"chat_id": chat_id},
-        files={"document": (file.name, file)},
-    )
+# upload backup file to drive
+file = file_upload(backup_name, "application/gz")
 
-os.remove(file_path)
+# send message to telegram
+message = f"Database backup: {file}"
+os.system(f"curl -s -X POST https://api.telegram.org/bot{BOT_TOKEN}/sendMessage -d chat_id={CHAT_ID} -d text='{message}'")
+
+# delete backup file from server
+os.remove(backup_name)
